41b68b330e630e2659cf646b8e575ba300b1130c
Kevin <kevin@pop-os.localdomain>
2026-02-26 00:48:12 +1300

    update

---
+++ update.sh
@@ -0,0 +1,14 @@
+
+cp ~/Downloads/yeva-advisor.jsx src/App.jsx
+
+npm version patch   # 1.0.0 → 1.0.1
+#npm version minor   # 1.0.0 → 1.1.0
+#npm version major   # 1.0.0 → 2.0.0
+
+npm run build
+
+cp dist/assets/*js ~/JSX/yeva-advisor-app/assets/index.js
+cp src/App.jsx ~/JSX/yeva-advisor-app/src/App.jsx
+
+sleep 3
+(cd ~/JSX/yeva-advisor-app && git add * && git commit -m 'Deploy' && git push)
