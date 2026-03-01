818a45292375164d79b71e9d97d5efdaa2bcae42
Kevin <kevin@pop-os.localdomain>
2026-03-01 22:28:08 +1300

    Updated

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 
 #cp ~/Downloads/yeva-advisor.jsx src/App.jsx
 
-npm version patch   # 1.0.0 → 1.0.1
-#npm version minor   # 1.0.0 → 1.1.0
+#npm version patch   # 1.0.0 → 1.0.1
+npm version minor   # 1.0.0 → 1.1.0
 #npm version major   # 1.0.0 → 2.0.0
 
 npm run build
